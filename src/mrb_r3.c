60d26a6238727afec187e1334400a42b8cc803c4
Sebastián Katzer <katzer.sebastian@googlemail.com>
2017-12-12 23:38:07 +0100

    Update headers

--- src/mrb_r3.c
+++ src/mrb_r3.c
@@ -1,6 +1,6 @@
 /* MIT License
  *
- * Copyright (c) Sebastian Katzer 2017
+ * Copyright (c) 2017 Sebastian Katzer
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
