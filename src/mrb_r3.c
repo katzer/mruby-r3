402bbe9b8009022cfae149cb8f844e87fd5dece7
Takeshi Watanabe <takechi101010@gmail.com>
2018-02-06 21:03:21 +0900

    Fix memory errors.

--- src/mrb_r3.c
+++ src/mrb_r3.c
@@ -23,6 +23,7 @@
 
 #include "mruby.h"
 #include "mruby/array.h"
+#include "mruby/class.h"
 #include "mruby/hash.h"
 #include "mruby/string.h"
 #include "mruby/data.h"
@@ -30,6 +31,16 @@
 #include "mruby/error.h"
 #include "memory.h"
 #include "r3.h"
+
+static void
+mrb_r3_tree_free(mrb_state *mrb, void *p)
+{
+    if (!p) { return; }
+
+    r3_tree_free((R3Node*)p);
+}
+
+static mrb_data_type const mrb_r3_tree_type = { "R3::Tree", mrb_r3_tree_free };
 
 static void
 mrb_r3_chomp_path(char *path, mrb_int *len)
@@ -56,9 +67,8 @@ mrb_r3_save_data(mrb_state *mrb, mrb_value self, mrb_value data)
 static mrb_value
 mrb_r3_f_init(mrb_state *mrb, mrb_value self)
 {
-    mrb_int capa;
+    mrb_int capa = 0;
     mrb_sym data_attr;
-    DATA_PTR(self) = NULL;
 
     mrb_get_args(mrb, "|i", &capa);
 
@@ -68,7 +78,7 @@ mrb_r3_f_init(mrb_state *mrb, mrb_value self)
     data_attr = mrb_intern_lit(mrb, "data");
     mrb_iv_set(mrb, self, data_attr, mrb_ary_new_capa(mrb, capa));
 
-    DATA_PTR(self) = r3_tree_create(capa);
+    mrb_data_init(self, r3_tree_create(capa), &mrb_r3_tree_type);
 
     return self;
 }
@@ -76,19 +86,20 @@ mrb_r3_f_init(mrb_state *mrb, mrb_value self)
 static mrb_value
 mrb_r3_f_add(mrb_state *mrb, mrb_value self)
 {
-    mrb_int path_len, method;
+    mrb_int path_len, method = 0;
     char *path;
     R3Node *tree = DATA_PTR(self);
-    mrb_value data;
+    mrb_value data = mrb_nil_value();
     mrb_bool data_given;
+    mrb_value path_str;
 
-    if (mrb_get_args(mrb, "s|io?", &path, &path_len, &method, &data, &data_given) == 1) {
-        method = 0;
-    }
+    mrb_get_args(mrb, "s|io?", &path, &path_len, &method, &data, &data_given);
 
-    path = strdup(path);
+    path_str = mrb_str_new_cstr(mrb, path);
+    path = mrb_str_to_cstr(mrb, path_str);
     mrb_r3_chomp_path(path, &path_len);
 
+    mrb_r3_save_data(mrb, self, path_str);
     if (data_given) {
         mrb_r3_save_data(mrb, self, data);
         r3_tree_insert_routel(tree, method, path, path_len, mrb_ptr(data));
@@ -119,15 +130,13 @@ mrb_r3_f_compile(mrb_state *mrb, mrb_value self)
 static mrb_value
 mrb_r3_f_matches(mrb_state *mrb, mrb_value self)
 {
-    mrb_int path_len, method;
+    mrb_int path_len, method = 0;
     char *path;
     R3Node *tree = DATA_PTR(self);
     match_entry *entry;
     R3Route *route;
 
-    if (mrb_get_args(mrb, "s|i", &path, &path_len, &method) == 1) {
-        method = 0;
-    }
+    mrb_get_args(mrb, "s|i", &path, &path_len, &method);
 
     path = strdup(path);
     mrb_r3_chomp_path(path, &path_len);
@@ -136,13 +145,11 @@ mrb_r3_f_matches(mrb_state *mrb, mrb_value self)
     entry->request_method = method;
 
     route = r3_tree_match_route(tree, entry);
+
     match_entry_free(entry);
+    free(path);
 
-    if (route) {
-        return mrb_true_value();
-    }
-
-    return mrb_false_value();
+    return mrb_bool_value(route? TRUE : FALSE);
 }
 
 static mrb_value
@@ -157,7 +164,7 @@ mrb_r3_f_mismatches(mrb_state *mrb, mrb_value self)
 static mrb_value
 mrb_r3_f_match(mrb_state *mrb, mrb_value self)
 {
-    mrb_int path_len, method, i, data_given = 0;
+    mrb_int path_len, method = 0, i, data_given = 0;
     char *path;
     R3Node *tree;
     R3Route *route;
@@ -165,9 +172,7 @@ mrb_r3_f_match(mrb_state *mrb, mrb_value self)
     r3_iovec_t *slugs, *tokens;
     mrb_value params, data, val, key;
 
-    if (mrb_get_args(mrb, "s|i", &path, &path_len, &method) == 1) {
-        method = 0;
-    }
+    mrb_get_args(mrb, "s|i", &path, &path_len, &method);
 
     path = strdup(path);
     mrb_r3_chomp_path(path, &path_len);
@@ -179,6 +184,7 @@ mrb_r3_f_match(mrb_state *mrb, mrb_value self)
 
     if (!route) {
         match_entry_free(entry);
+        free(path);
         return mrb_nil_value();
     }
 
@@ -199,6 +205,7 @@ mrb_r3_f_match(mrb_state *mrb, mrb_value self)
     }
 
     match_entry_free(entry);
+    free(path);
 
     if (data_given == 0)
         return params;
@@ -221,6 +228,7 @@ mrb_r3_f_free(mrb_state *mrb, mrb_value self)
     mrb_iv_remove(mrb, self, data);
     r3_tree_free(tree);
     DATA_PTR(self) = NULL;
+    DATA_TYPE(self) = NULL;
 
     return mrb_true_value();
 }
@@ -241,6 +249,7 @@ mrb_mruby_r3_gem_init(mrb_state *mrb)
     mrb_define_const(mrb, r3, "OPTIONS", mrb_fixnum_value(METHOD_OPTIONS));
 
     tr = mrb_define_class_under(mrb, r3, "Tree", mrb->object_class);
+    MRB_SET_INSTANCE_TT(tr, MRB_TT_DATA);
     mrb_define_method(mrb, tr, "initialize", mrb_r3_f_init, MRB_ARGS_OPT(1));
     mrb_define_method(mrb, tr, "add",        mrb_r3_f_add, MRB_ARGS_ARG(1,2));
     mrb_define_method(mrb, tr, "<<",         mrb_r3_f_add, MRB_ARGS_ARG(1,2));
